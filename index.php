5fbe40ac8028de7a87c4df1abcc72d82401601f8
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-18 13:45:00 -0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Name Greeting Form</title>
+</head>
+
+<body>
+    <h1>PHP Name Greeting Form</h1>
+    <p>Create a simple HTML form and accept the user name and display the name through PHP echo statement.</p>
+
+
+</body>
+
+</html>
