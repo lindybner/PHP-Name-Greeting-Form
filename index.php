de50f154ee36faec9078cfdec1333d69a066102d
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-18 14:00:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
     <h1>PHP Name Greeting Form</h1>
     <p>Create a simple HTML form and accept the user name and display the name through PHP echo statement.</p>
 
+    <form method="POST">
+        <div>
+            <label for="name">Please input your name:</label>
+        </div>
+        <div>
+            <input type="text" name="name" id="name">
+        </div>
+        <input type="submit" value="Submit Name">
+    </form>
+
+    <?php
+    // Check if the form is submitted with the POST method and if 'name' is set in $_POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name'])) {
+        // Retrieve the 'name' field from the form and store it in a variable
+        $name = $_POST['name'];
+
+        // Display a greeting message using the entered name
+        echo "<h2>Greetings " . $name . "!</h2>";
+    }
+    ?>
 
 </body>
 
